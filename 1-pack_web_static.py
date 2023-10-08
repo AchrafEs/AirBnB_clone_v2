4598dbbec18e8cb28ccbee3cd18abe854425217a
AchrafEs <achraf.es2110@gmail.com>
2023-10-08 13:42:41 +0000

    updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-a Fabric script that generates a .tgz archive from the contents
+A Fabric script that generates a .tgz archive from the contents
 of the web_static folder of your AirBnB Clone repo,
 using the function do_pack.
 """
 from fabric.api import local
 from datetime import datetime
-
+import os
 
 def do_pack():
     try:
         # Create the 'versions' directory if it doesn't exist
-        local("mkdir -p versions")
+        if not os.path.exists("versions"):
+            os.mkdir("versions")
 
         # Generate the name for the archive
         current_time = datetime.now()
@@ -25,12 +26,28 @@ def do_pack():
         )
 
         # Create the .tgz archive using tar
+        print("Packing web_static to versions/{}".format(archive_name))
         local(
-                "tar -cvzf versions/{} web_static".format(archive_name),
-                capture=False
-                )
+            "tar -cvzf versions/{} web_static".format(archive_name),
+            capture=False
+        )
 
-        # Return the path to the archive
-        return "versions/{}".format(archive_name)
-    except Exception:
+        archive_path = "versions/{}".format(archive_name)
+
+        # Check if the archive was created successfully
+        if os.path.exists(archive_path):
+            archive_size = os.path.getsize(archive_path)
+            print("web_static packed: {} -> {}Bytes".format(archive_path, archive_size))
+            return archive_path
+        else:
+            return None
+    except Exception as e:
         return None
+
+# Test the function
+if __name__ == "__main__":
+    result = do_pack()
+    if result:
+        print("Archive created:", result)
+    else:
+        print("Failed to create archive.")
