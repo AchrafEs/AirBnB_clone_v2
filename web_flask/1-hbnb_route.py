5669166ffd2ed2b3153d4aaec97a15cc03dffdd2
AchrafEs <achraf.es2110@gmail.com>
2023-10-22 16:19:00 +0000

    updated the script

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application
+"""
+A script that starts a Flask web application
 """
 
 from flask import Flask
@@ -9,13 +10,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ dispalys Hello HBNB! """
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb' strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ displays 'HBNB' """
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
